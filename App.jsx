4509561ae2520bc02c32a3f6c6616e1152e0e2dd
favas-cv <fawazmuhd0@gmail.com>
2025-08-20 16:07:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Theme from './componetsfortask/Theme'
+
+function App() {
+  return(
+    <>
+    <Theme/>
+    </>
+  )
+}
+
+export default App
